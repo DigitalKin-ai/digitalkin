c5621248721598d4e4c030874d09b38fcb4c83fb
Thibaud Perrin <thibaud.perrin6@gmail.com>
2025-03-25 10:05:24 +0100

    fix(ci): fix ci, remove --no-deps, and dupplicate actions fix(taskfile): update python version and taskfile fix(config): clean bumpveersion config fix(version): fix version file Bump version: 0.0.1 → 0.1.0 fix(version): fix version file

--- src/digitalkin/__version__.py
+++ src/digitalkin/__version__.py
@@ -1,3 +1,11 @@
 """Version information."""
 
-__version__ = "0.0.1"
+import os
+import sys
+from importlib.metadata import PackageNotFoundError, version
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+try:
+    __version__ = version("digitalkin")
+except PackageNotFoundError:
+    __version__ = "0.1.0"
