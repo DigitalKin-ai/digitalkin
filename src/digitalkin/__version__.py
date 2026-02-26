e74e000c2d9c9e3a64621aa99678456a1732044f
Guillaume Sachet <contact@guillaumesachet.fr>
2026-02-26 14:38:17 +0100

    chore(dependencies): downgrade grpcio packages to version 1.78.0 in configuration files
    chore(version): bump version to 0.3.2b9 in configuration files

--- src/digitalkin/__version__.py
+++ src/digitalkin/__version__.py
@@ -5,4 +5,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("digitalkin")
 except PackageNotFoundError:
-    __version__ = "0.3.2b8"
+    __version__ = "0.3.2b9"
